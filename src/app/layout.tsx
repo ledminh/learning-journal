5be6c2d5aef633c0e356bbbfd6c04453f38b91ea
Minh Le <duyminh.le310@gmail.com>
2023-12-01 16:05:34 -0500

    Fix layout for responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,15 +21,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="w-11/12 mx-auto pt-4 min-h-screen max-w-4xl flex flex-col justify-between">
-          <div className="grid grid-cols-7 gap-10">
+          {/* Header, Body, and SideBar here*/}
+          <div className="flex flex-col gap-6">
             <header className="col-span-7">
               <Header />
             </header>
-            {/* @TODO: Make this responsive. In small screen, make aside on top of main */}
-            <main className="col-span-5">{children}</main>
-            <aside className="col-span-2">
-              <SideBar />
-            </aside>
+            {/* Body and SideBar here*/}
+            <div className="grid grid-cols-7 gap-10">
+              <main className="col-span-7 order-2 sm:order-1 sm:col-span-5">
+                {children}
+              </main>
+              <aside className="col-span-7 order-1 sm:order-2 sm:col-span-2">
+                <SideBar />
+              </aside>
+            </div>
           </div>
           <footer>
             <Footer />
