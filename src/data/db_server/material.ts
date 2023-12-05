34c19cc446c884203c72d46b59b5245b3d729773
Minh Le <duyminh.le310@gmail.com>
2023-12-04 19:21:58 -0500

    fix .env leak

---
+++ src/data/db_server/material.ts
@@ -0,0 +1,1 @@
+// database API for material here
--- src/types/material.ts
+++ src/types/material.ts
@@ -1,3 +1,5 @@
+import { AsyncFunction } from "./";
+
 export enum MaterialType {
   Link = "MaterialType/link",
   Quote = "MaterialType/quote",
@@ -6,6 +8,39 @@ export enum MaterialType {
 }
 
 export type Material = {
+  id: string;
   type: MaterialType;
   content: string;
 };
+
+/************************************************
+ * CRUD
+ ************************************************/
+
+/******** CREATE **********************/
+
+export type DataToCreateMaterial = Omit<
+  | {
+      type: MaterialType.Link | MaterialType.Quote | MaterialType.Code;
+      content: string;
+    }
+  | {
+      type: MaterialType.Image;
+      content: File;
+    },
+  "id"
+>;
+
+export type CreateMaterialFunction = AsyncFunction<
+  DataToCreateMaterial,
+  Material
+>;
+
+/******** READ **********************/
+// Material is always read together with journal entry.
+
+/******** UPDATE **********************/
+export type UpdateMaterialFunction = AsyncFunction<
+  DataToCreateMaterial,
+  Material
+>;
