fb6864c062e5eeb973ab374a091fde77eab408ac
Minh Le <minh.le@ledminh.dev>
2023-12-26 16:36:52 -0500

    test

--- src/data/server/supabase.ts
+++ src/data/server/supabase.ts
@@ -4,8 +4,8 @@ let supabase: SupabaseClient | null = null;
 
 if (supabase === null) {
   supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    process.env.SUPABASE_URL as string,
+    process.env.SUPABASE_API_KEY as string,
     {
       auth: { persistSession: false },
     }
