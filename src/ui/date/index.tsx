ef77350aecb54e33e6b20d1f0fc47fa3ff901560
Minh Le <minh.le@ledminh.dev>
2024-01-02 09:28:11 -0500

    DateBlock

--- src/ui/date/index.tsx
+++ src/ui/date/index.tsx
@@ -12,20 +12,28 @@ export async function DateBlock() {
   return (
     <Block title="Date">
       {errorMessage && <div className="text-red-500">{errorMessage}</div>}
-
-      {dateEntries &&
-        dateEntries.map((dateEntry) => (
-          <Link
-            href={`/date/${formatDateString(dateEntry.date)}`}
-            key={dateEntry.id}
-            className="font-mono text-blue-800 underline"
-          >
-            {formatDateString(dateEntry.date)}
+      {dateEntries && (
+        <div className="flex flex-col gap-4">
+          <ul>
+            {dateEntries.map((dateEntry) => (
+              <li key={dateEntry.id}>
+                <Link
+                  href={`/date/${formatDateString(dateEntry.date).replace(
+                    /[ ,]/g,
+                    "-"
+                  )}`}
+                  className="font-mono underline"
+                >
+                  {formatDateString(dateEntry.date)}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Link href="/dates" className="font-mono text-blue-800 underline">
+            View all dates
           </Link>
-        ))}
-      <Link href="/dates" className="font-mono text-blue-800 underline">
-        View all dates
-      </Link>
+        </div>
+      )}
     </Block>
   );
 }
--- src/ui/tag/index.tsx
+++ src/ui/tag/index.tsx
@@ -13,23 +13,24 @@ export async function TagBlock() {
           <div className="p-2 font-mono text-sm bg-red-100">{errorMessage}</div>
         )}
         {payload && (
-          <ul className="flex flex-wrap gap-2">
-            {payload.map((tag) => (
-              <li key={tag.id}>
-                <Link
-                  href={`/tag/${tag.slug}`}
-                  className="px-2 py-1 font-mono text-sm bg-blue-100 rounded-md hover:bg-blue-300"
-                >
-                  {tag.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <div className="flex flex-col gap-4">
+            <ul className="flex flex-wrap gap-2">
+              {payload.map((tag) => (
+                <li key={tag.id}>
+                  <Link
+                    href={`/tag/${tag.slug}`}
+                    className="px-2 py-1 font-mono text-sm bg-blue-100 rounded-md hover:bg-blue-300"
+                  >
+                    {tag.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Link href="/tags" className="font-mono text-blue-800 underline">
+              View all tags
+            </Link>
+          </div>
         )}
-
-        <Link href="/tags" className="font-mono text-blue-800 underline">
-          View all tags
-        </Link>
       </div>
     </Block>
   );
