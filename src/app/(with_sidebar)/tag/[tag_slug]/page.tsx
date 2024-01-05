f36a058d143653ef1d87c89b60a125fefbbe28ec
Minh Le <minh.le@ledminh.dev>
2024-01-04 22:03:53 -0500

    Add data to tag page

--- src/app/(with_sidebar)/tag/[tag_slug]/page.tsx
+++ src/app/(with_sidebar)/tag/[tag_slug]/page.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
 import JournalEntry from "@/ui/journal_entry";
+import { getTag } from "@/data/api/tag";
 
-export default function TagPage(props: { params: { tag_name: string } }) {
+export default async function TagPage(props: { params: { tag_slug: string } }) {
+  const { errorMessage, payload } = await getTag({
+    slug: props.params.tag_slug,
+  });
+
+  if (errorMessage) {
+    return (
+      <div className="p-2 font-mono text-sm bg-red-100">{errorMessage}</div>
+    );
+  }
+
+  if (!payload) {
+    return <div className="p-2 font-mono text-sm bg-red-100">No tag found</div>;
+  }
+
   return (
     <>
-      <p>Tag {props.params.tag_name}</p>
+      <p>Tag {payload.name}</p>
       <ul className="flex flex-col gap-2">
-        {Array.from({ length: 3 }).map((_, i) => (
-          <li key={i}>
-            <Link href="/entry/01">
-              <JournalEntry type="summary" />
+        {payload.journalEntries.map((jE) => (
+          <li key={jE.id}>
+            <Link href={`/entry/${jE.slug}`}>
+              <JournalEntry type="summary" journalEntry={jE} />
             </Link>
           </li>
         ))}
