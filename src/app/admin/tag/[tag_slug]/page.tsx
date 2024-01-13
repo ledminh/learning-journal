d1fa2575e6da9fa90ba9245adf2520f4d67480b3
Minh Le <minh.le@ledminh.dev>
2024-01-12 22:08:18 -0500

    admin/tag/ page content

--- src/app/admin/tag/[tag_slug]/page.tsx
+++ src/app/admin/tag/[tag_slug]/page.tsx
@@ -1,17 +1,76 @@
-import JournalList from "@/ui/admin/JournalList/JournalList";
+import { getTag } from "@/data/api/tag";
+import Link from "next/link";
+import { JournalEntry } from "@/data/server/types/journal_entry";
 
-export default function TagPage() {
+interface Props {
+  params: {
+    tag_slug: string;
+  };
+}
+
+export default async function TagPage({ params }: Props) {
+  const { errorMessage, payload } = await getTag({ slug: params.tag_slug });
+
+  if (errorMessage) {
+    return <div className="p-2 font-mono bg-red-200">{errorMessage}</div>;
+  }
+
+  if (!payload) {
+    return (
+      <div>
+        <p className="font-mono bg-red-200">Cannot load payload</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4">
-      <section className="flex gap-4 justify-start items-center">
-        <h2 className="font-semibold text-lg">Tag 1</h2>
-        <button className="bg-neutral-500 py-1 px-2 text-white">rename</button>
-        <button className="bg-red-900 py-1 px-2 text-white">empty</button>
-        <button className="bg-red-900/50 py-1 px-2 text-white">delete</button>
+      <section className="flex flex-wrap items-center justify-start gap-10">
+        <h2 className="text-xl font-semibold text-blue-800 border-b-4 border-b-blue-800">
+          TAG: {payload.tag.name}
+        </h2>
+        <div className="flex items-center justify-start gap-2">
+          <button className="px-2 py-1 text-white bg-neutral-500">
+            rename
+          </button>
+          <button className="px-2 py-1 text-white bg-red-900">empty</button>
+          <button className="px-2 py-1 text-white bg-red-900/50">delete</button>
+        </div>
       </section>
-      <section>
-        <JournalList />
+      <section className="flex flex-col gap-4">
+        <h3 className="text-lg font-semibold">Journal Entries</h3>
+        <JournalList journalEntries={payload.tag.journalEntries} />
       </section>
     </div>
   );
 }
+
+/***************************
+ * Components
+ */
+
+const JournalList: React.FC<{ journalEntries: JournalEntry[] }> = ({
+  journalEntries,
+}) => {
+  return (
+    <ul className="flex flex-col gap-2">
+      {journalEntries.length > 0 ? (
+        journalEntries.map((journalEntry, index) => (
+          <li key={journalEntry.id}>
+            <Link
+              href={`/admin/edit-journal/${journalEntry.slug}`}
+              className={`flex items-center justify-between p-2 hover:bg-blue-400 ${
+                index % 2 === 0 ? "bg-blue-100" : "bg-blue-200"
+              }`}
+            >
+              <span className="font-semibold">{journalEntry.title}</span>
+              <span>{journalEntry.createdAt.toLocaleDateString("en-US")}</span>
+            </Link>
+          </li>
+        ))
+      ) : (
+        <li className="p-2 font-mono bg-red-200">No journal entries found</li>
+      )}
+    </ul>
+  );
+};
