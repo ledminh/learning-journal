179c579ac9d2d7fe28ee913368a8becd672c35df
Minh Le <minh.le@ledminh.dev>
2024-01-13 15:51:11 -0500

    Decorate Footer

--- src/ui/layout/Footer.tsx
+++ src/ui/layout/Footer.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <p className="text-sm">Minh Le</p>;
+  return (
+    <div className="pt-4 my-4 border-t-2 border-t-neutral-700">
+      <span>{new Date().getFullYear()} © </span>
+      <Link href="https://www.ledminh.dev" className="underline">
+        Minh Le
+      </Link>
+    </div>
+  );
 }
